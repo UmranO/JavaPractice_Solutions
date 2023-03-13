903107758169405654557bc0fac6f7f5f6065144
Umran Ozman <umonal@yahoo.com>
2023-03-13 16:57:50 -0400

    OxygenTank Class updated

--- src/day06_ifStatements/OxygenTank.java
+++ src/day06_ifStatements/OxygenTank.java
@@ -1,8 +1,7 @@
 package day06_ifStatements;
-
 public class OxygenTank {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         int number = 80;
         String result = "";
 
@@ -18,7 +17,7 @@ public class OxygenTank {
             result = "Don't go too far";
         }
 
-        if(number < 70 && number >= 60){
+        if(number < 70 &&number >= 60){
             result = "Start to head back";
         }
 
@@ -30,13 +29,13 @@ public class OxygenTank {
     }
 
 }
+
 /*
-        Create a class named OxygenTank. You are diving in the ocean. Your oxygen tank has a certain level (number) and
-        you must print a message based on the level:
-        Above 90 -  Your tank is full
-        Above 80 -  Still okay
-        Above 70 -  Don't go too far
-        Above 60 -  Start to head back
-        Above 50 -  Be careful now you are at 50%
-        USE ONE PRINT STATEMENT ONLY
-        */
+Create a class named OxygenTank. You are diving in the ocean. Your oxygen tank has a certain level (number) and you must print a message based on the level:
+                Above 90 -  Your tank is full
+                Above 80 -  Still okay
+                Above 70 -  Don't go too far
+                Above 60 -  Start to head back
+                Above 50 -  Be careful now you at at 50%
+                USE ONE PRINT STATEMENT ONLY
+ */
