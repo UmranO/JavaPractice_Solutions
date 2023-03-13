2310cf17c4a54373885972318be635d1385f5439
Umran Ozman <umonal@yahoo.com>
2023-03-13 11:44:35 -0400

    AboutMe Class created

---
+++ src/day02_escapeSequences/AboutMe.java
@@ -0,0 +1,10 @@
+package day02_escapeSequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("My favorite music is \"Rock\" musics. \nMy favorite book is \"Data Structure in Java\"");
+
+    }
+}
