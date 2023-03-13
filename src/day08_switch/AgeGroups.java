e483134f26a10f2cf360b66c96154d38f2a17675
Umran Ozman <umonal@yahoo.com>
2023-03-13 19:56:36 -0400

    AgeGroups updated

--- src/day08_switch/AgeGroups.java
+++ src/day08_switch/AgeGroups.java
@@ -7,7 +7,7 @@ public class AgeGroups {
 
         if (age >= 0 && age <= 150) {
 
-            result =  (age <= 2) ? "Infant" : (age <= 5) ? "Toddler" : (age <= 9) ? "Kid" : (age <= 12) ? "Pre-Teen"
+            result =  (age <= 2)  ? "Infant" : (age <= 5) ? "Toddler" : (age <= 9) ? "Kid" : (age <= 12) ? "Pre-Teen"
                     : (age <= 17) ? "Teenager" : (age <= 20) ? "Young Adult" : (age <= 39) ? "Adult"
                     : (age <= 49) ? "Young Middle-Aged Adult" : (age <= 54) ? "Middle-Aged Adult"
                     : (age <= 64) ? "Very Young Senior Citizen" : (age <= 74) ? "Young Senior Citizen"
