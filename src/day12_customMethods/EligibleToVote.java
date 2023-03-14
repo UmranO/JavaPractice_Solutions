f8055220971717aec2f48171da6f4c8e156eae08
Umran Ozman <umonal@yahoo.com>
2023-03-14 18:04:02 -0400

    EligibleToVote updated

--- src/day12_customMethods/EligibleToVote.java
+++ src/day12_customMethods/EligibleToVote.java
@@ -1,4 +1,26 @@
 package day12_customMethods;
 
 public class EligibleToVote {
+
+//----UO Way--------------------------------------------------------------------------------------------------
+    public static void eligibleToVote(int age, boolean isAmerican){  //is American ismi degerini cagirdiginda veriyorsun
+      if (age>=18 && isAmerican){
+          System.out.println(" You are eligible to vote");
+      }else System.out.println( "You are ineligible to vote");
+
+    }
+//----Test----------------------------------------------------------------------------------------------------
+public static void main(String[] args) {
+    eligibleToVote(19,false);
 }
+}
+/*
+6. Create a method named eligibleToVote that takes two arguments:
+        1. age (int)
+        2. isAmerican (boolean)
+    Then the method should determine if the person eligible to vote
+        Ex:
+            eligibleToVote(23, true)
+        output:
+            You are eligible to vote
+ */
