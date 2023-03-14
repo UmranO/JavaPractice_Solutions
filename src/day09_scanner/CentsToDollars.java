33faa00ec1ebdf4a3049a4da071f66fd44510187
Umran Ozman <umonal@yahoo.com>
2023-03-14 10:16:06 -0400

    CentsToDollars updated

--- src/day09_scanner/CentsToDollars.java
+++ src/day09_scanner/CentsToDollars.java
@@ -1,4 +1,15 @@
 package day09_scanner;
 
 public class CentsToDollars {
+    public static void main(String[] args) {
+
+    }
 }
+/*
+3. Write a program that can convert cents to dollars, if there is any remainder include them in the result as cents
+            Ex:
+                Enter cents
+                225
+                output:
+                225 cents equal to: 2 dollars and 25 cents
+ */
