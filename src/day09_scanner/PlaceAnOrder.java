86cd39130860f66196851bc02b51f89a8b9f37bf
Umran Ozman <umonal@yahoo.com>
2023-03-14 12:11:05 -0400

    PlaceAnOrder updated

--- src/day09_scanner/PlaceAnOrder.java
+++ src/day09_scanner/PlaceAnOrder.java
@@ -1,8 +1,21 @@
 package day09_scanner;
 
+import java.util.Scanner;
+
 public class PlaceAnOrder {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
 
+        System.out.println("enter the product name");
+        String product=input.nextLine();
+        System.out.println("enter the price");
+        double price=input.nextDouble();
+        System.out.println("enter the quantity");
+        int quantity=input.nextInt();
+        System.out.println("enter their first name");
+        String fName=input.next();
+        input.close();
+        System.out.println(fName+", your order for "+quantity+" "+product+" has been placed. Your total is $"+quantity*price);
     }
 }
 /*
@@ -15,5 +28,5 @@ public class PlaceAnOrder {
                 Ex:
                     Input: "Apples" , 1.5, 5. "Luke"
                     Output:
-                        Luke, your order for 5 Apples has been placed. Your total is 7.5.
+                        Luke, your order for 5 Apples has been placed. Your total is $7.5.
  */
