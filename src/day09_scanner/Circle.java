66e5719bb4fbf8eb3210d2ef54b4cb1537f048b2
Umran Ozman <umonal@yahoo.com>
2023-03-14 10:16:19 -0400

    Circle updated

--- src/day09_scanner/Circle.java
+++ src/day09_scanner/Circle.java
@@ -1,4 +1,12 @@
 package day09_scanner;
 
 public class Circle {
+    public static void main(String[] args) {
+
+    }
 }
+/*
+2. Create a class named Circle:
+            1.1 Ask the user to enter the radius of the circle
+            1.1 Calculate the area and perimeter of the circle by using the radius
+ */
