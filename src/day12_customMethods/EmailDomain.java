b24818b1c58128123a89388441e2b618e81a1dc9
Umran Ozman <umonal@yahoo.com>
2023-03-14 17:58:15 -0400

    EmailDomain updated

--- src/day12_customMethods/EmailDomain.java
+++ src/day12_customMethods/EmailDomain.java
@@ -1,4 +1,21 @@
 package day12_customMethods;
 
 public class EmailDomain {
+//----UO Way--------------------------------------------------------------------------------------------------
+    public static void emailDomain(String email){
+        String domain=email.substring(email.indexOf("@")+1,email.indexOf("."));
+        System.out.println(domain);
+    }
+//----Test----------------------------------------------------------------------------------------------------
+    public static void main(String[] args){
+        emailDomain("umranz@gmail.com");
+
+    }
 }
+/*
+1. Create a method named emailDomain that can display the domain of the email
+    Ex:
+        emailDomain("Cydeo@gmail.com")
+        output:domain: gmail
+
+ */
