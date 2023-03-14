c875221626e6b3747e21c1bcef596a4464af9b8b
Umran Ozman <umonal@yahoo.com>
2023-03-14 11:10:33 -0400

    GradeReport updated

--- src/day09_scanner/GradeReport.java
+++ src/day09_scanner/GradeReport.java
@@ -1,8 +1,15 @@
 package day09_scanner;
-
+import java.util.Scanner;
 public class GradeReport {
     public static void main(String[] args) {
 
+        System.out.println("Enter your score");
+        int score=new Scanner(System.in).nextInt();
+
+        String grade= (score<=100 && score>0)?
+                (score>=90)?"A" :(score>=80)?"B" :(score>=70)?"C" :(score>=60)?"D" :"F"
+                : "Invalid score";
+        System.out.println(grade);
     }
 }
 /*
