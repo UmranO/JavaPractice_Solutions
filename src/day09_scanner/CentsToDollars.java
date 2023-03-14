078c149ed73558b15919d08ec94c84c705db562c
Umran Ozman <umonal@yahoo.com>
2023-03-14 10:32:20 -0400

    CentsToDollars updated

--- src/day09_scanner/CentsToDollars.java
+++ src/day09_scanner/CentsToDollars.java
@@ -1,7 +1,26 @@
 package day09_scanner;
-
+import java.util.Scanner;
 public class CentsToDollars {
     public static void main(String[] args) {
+
+        Scanner input=new Scanner (System.in);
+//----UO Way---------------------------------------------------------------------------------------------------------
+        System.out.println("Enter an amount");
+        int cent=input.nextInt();
+        System.out.println(cent +" cents equal to: "+(cent/100)+" dollars and " + (cent%100)+" cents");
+
+//----M Way---------------------------------------------------------------------------------------------------------
+        System.out.println("Enter cents");
+        int cents = input.nextInt();
+
+        input.close(); // we no longer need scanner after this line
+
+        int dollar = cents / 100;
+
+        cents -= dollar * 100; // or: cents = cents - (dollar * 100)
+
+        System.out.println(dollar+" dollars and "+cents+" cents");
+
 
     }
 }
