f2b0b3ce3df14c169e8006541994dedd93c768f8
Umran Ozman <umonal@yahoo.com>
2023-03-14 10:17:25 -0400

    PlaceAnOrder updated

--- src/day09_scanner/PlaceAnOrder.java
+++ src/day09_scanner/PlaceAnOrder.java
@@ -1,4 +1,19 @@
 package day09_scanner;
 
 public class PlaceAnOrder {
+    public static void main(String[] args) {
+
+    }
 }
+/*
+6. PlaceAnOrder task:
+                Ask User to enter the product name (multiple words)
+                Ask the user to enter the price  (double)
+                Ask the user to enter the quantity (int)
+                Ask the user to enter their first name (String, single word)
+            Print in the following format:
+                Ex:
+                    Input: "Apples" , 1.5, 5. "Luke"
+                    Output:
+                        Luke, your order for 5 Apples has been placed. Your total is 7.5.
+ */
