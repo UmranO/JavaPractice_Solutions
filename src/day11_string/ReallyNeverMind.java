b12188cca61f32dc6928e0ec65bb829943daf554
Umran Ozman <umonal@yahoo.com>
2023-03-14 14:51:48 -0400

    ReallyNeverMind updated

--- src/day11_string/ReallyNeverMind.java
+++ src/day11_string/ReallyNeverMind.java
@@ -1,4 +1,21 @@
 package day11_string;
 
 public class ReallyNeverMind {
+    public static void main(String[] args) {
+        String w="rasdfgly";
+        if (w.endsWith("ly")) System.out.println("really?");
+        else System.out.println("never mind");
+
+//M Way:String lastTwoChars = word.substring( word.length()-2 ); //kelimenin sondan 2.sinden baslayan substring yaratmis
+//if(lastTwoChars.equals("ly")){
+//            System.out.println("really???");
+//        }else{
+//            System.out.println("Never mind");
+//        }
+
+    }
 }
+/*
+4. Create a class named ReallyNeverMind. Ask the user to enter a word.
+if the word ends with "ly", print "really???" ,  otherwise, print "never mind"
+ */
