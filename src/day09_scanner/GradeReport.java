d8572b0a5316127d681e8aff52eb7f78e5d21777
Umran Ozman <umonal@yahoo.com>
2023-03-14 10:16:33 -0400

    GradeReport  updated

--- src/day09_scanner/GradeReport.java
+++ src/day09_scanner/GradeReport.java
@@ -1,4 +1,13 @@
 package day09_scanner;
 
 public class GradeReport {
+    public static void main(String[] args) {
+
+    }
 }
+/*
+3. Create a class named GradeReport:
+            2.1 Ask the user to enter the his/her score
+            2.2 Print the grade of the student (A, B, C, D, F)
+            2.3 If user enter invalid score (negative or more than 100) print invalid score
+ */
