d381a4b263e9d0ebd1146f2d3735df060075aac0
Umran Ozman <umonal@yahoo.com>
2023-03-14 17:20:51 -0400

    SalaryCalculator edited

--- src/day12_customMethods/SalaryCalculator.java
+++ src/day12_customMethods/SalaryCalculator.java
@@ -1,4 +1,32 @@
 package day12_customMethods;
 
 public class SalaryCalculator {
+    public static void calculateSalary(double hourlyRate, int weeklyHours){
+        double salary = hourlyRate * weeklyHours * 52;
+
+        System.out.println("You make $"+hourlyRate +" per hour" +
+                          "\nYou work "+ weeklyHours+" hours per week" +
+                          "\nYour gross income is $"+salary );
+    }
+
+    public static void main(String[] args) {
+        calculateSalary(12.0,3);
+    }
 }
+
+
+
+
+
+/*
+    Create a method named salary that takes two arguments:
+        1. hourlyRate (double)
+        2. weeklyHours (int)
+    Then the method should display the salary of the person.
+        Ex:
+            salary(45, 40)
+        output:
+            You make $45.0 per hour
+            You work 40 hours per week
+            Your gross income is $93600.0
+     */
