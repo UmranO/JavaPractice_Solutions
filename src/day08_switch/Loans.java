3b5c0eb4256800f9ba4fe8bbe0f59506ef3e01fc
Umran Ozman <umonal@yahoo.com>
2023-03-13 22:40:01 -0400

    Loans updated

--- src/day08_switch/Loans.java
+++ src/day08_switch/Loans.java
@@ -1,4 +1,20 @@
 package day08_switch;
 
 public class Loans {
+    public static void main(String [] args){
+        int salary=600045;
+        int creditScore=651;
+
+        System.out.println((salary>60000&&creditScore>650)? "Loan Approved" :"Loan Denied");
+
+    }
 }
+/*
+2. Create a class called Loans, Given two variables salary and credit score, use those given info to determine if
+   you can get a loan.
+                To be approved for the loan, print: "Loan Approved":
+                        Salary: above 60,000
+                        Credit Score: above 650
+                    Otherwise, print: "Loan Denied"
+              NOTE: MUST USE TERNARY.
+ */
