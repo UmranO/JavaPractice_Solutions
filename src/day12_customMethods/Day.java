7aa15b83033173ab0e5d57ce87a93d05dc7bb2c9
Umran Ozman <umonal@yahoo.com>
2023-03-14 17:04:39 -0400

    Day created

---
+++ src/day12_customMethods/Day.java
@@ -0,0 +1,11 @@
+package day12_customMethods;
+
+public class Day {
+}
+/*
+Create a method named day that can display the name of the day based on the given number to the method, if the number is invalid, then the method should print the error message "Invalid Number"
+    Ex:
+        day(5)
+        output:
+            Friday
+ */
