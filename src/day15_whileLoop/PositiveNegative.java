ad4d64fbef955864f944017fee642a00c8ee7981
Umran Ozman <umonal@yahoo.com>
2023-03-15 14:14:06 -0400

    PositiveNegative Class updated

--- src/day15_whileLoop/PositiveNegative.java
+++ src/day15_whileLoop/PositiveNegative.java
@@ -1,7 +1,5 @@
 package day15_whileLoop;
-
 import java.util.Scanner;
-
 public class PositiveNegative {
     public static void main(String[] args){
        Scanner input=new Scanner(System.in);
@@ -19,13 +17,6 @@ public class PositiveNegative {
 /*
 1. Write a program that asks user to enter number for 5 times, and print how many positive and negative numbers user
 entered
-			Ex:
-				Inputs:
-					10
-					20
-					-1
-					0
-					3
-				Output:
-					3 positive and 1 negative
- */
+			Ex: Inputs:10, 20, -1, 0, 3
+				Output:3 positive and 1 negative
+				*/
