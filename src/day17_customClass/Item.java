99318e8914a1e90857a1f1227671036301a32966
Umran Ozman <umonal@yahoo.com>
2023-03-18 14:57:04 -0400

    Item Class updated

--- src/day17_customClass/Item.java
+++ src/day17_customClass/Item.java
@@ -1,4 +1,42 @@
 package day17_customClass;
-
 public class Item {
+    public String name;
+    public double unitPrice;
+    public int quantity;
+
+    public void setInfo(String name, double unitPrice, int quantity){
+        this.name=name;
+        this.unitPrice=unitPrice;
+        this.quantity=quantity;
+    }
+    public double calcCost(){
+        return unitPrice*quantity;
+    }
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", unitPrice= $" + unitPrice +
+                ", quantity=" + quantity +
+                ", total Price= $" + calcCost() +
+                '}';
+    }
+
+    //----Test-----------------------------------------------------------------------------------------------------------
+public static void main(String[] args) {
+    Item item=new Item();
+    item.setInfo("tomato", 3.2,1);
+    System.out.println(item.toString());
+    System.out.println(item.calcCost());
 }
+
+}
+/*
+Create a class called Item
+    Attributes:
+        name, unitPrice, quantity
+    Actions:
+    	setInfo(): sets all the fields of Item object
+        calcCost(): returns the total price of the Item
+        toString(): when a Item object is passed in print statement,
+            it should display the full information of the item object
+ */
